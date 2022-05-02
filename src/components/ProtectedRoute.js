d56a52b100ff1e1289adb467041b6804d55793f7
delioncourts <delioncourtles@yandex.ru>
2022-05-02 19:50:14 +0300

    upd protectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,17 +1,13 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { CurrentUserContext } from '../contexts/CurrentUserContext'; // импортируем контекст
+import { Route, Redirect } from "react-router-dom"
 
-// этот компонент принимает другой компонент в качестве пропса
-// он также может взять неограниченное число пропсов и передать их новому компоненту
-const ProtectedRoute = ({ component: Component, ...props }) => {
-    const value = React.useContext(CurrentUserContext); // получаем значения из контекста
-    return (
-      <Route>
-        {
-          () => value.state.loggedIn === true ? <Component {...props} userData={value.state.userData} /> : <Redirect to="./login" />
-        }
-      </Route>
-  )}
+function ProtectedRoute({ component: Component, ...props }) {
+  return (
+    <Route>
+      {
+        () => (props.loggedIn ? <Component {...props} /> : <Redirect to='/sign-in' />)
+      }
+    </Route>
+  )
+}
 
 export default ProtectedRoute;
