2cd25a5dce6a88d21b04ef90fdb60dc7da0798d7
delioncourts <delioncourtles@yandex.ru>
2022-05-02 19:38:05 +0300

    upd logos, infoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,2 +1,21 @@
+import successLogo from "../images/SuccessLogo.svg"
+import failLogo from "../images/FailLogo.svg";
+
+export default function InfoTooltip(props) {
+    return (
+        <div className={`popup ${props.isOpen && "popup_opened"}`}>
+            <div className="popup__container-open">
+                <button className="popup__close" aria-label="закрыть" type="button" onClick={props.onClose}></button>
+                <img className="popup__open-photo" alt="#" src={props.status === "success" ? successLogo : failLogo} />
+                <h2 className="popup__open-photo-subtitle">
+                    {props.status === "success"
+                        ? "Вы успешно зарегистрировались!"
+                        : "Что-то пошло не так! Попробуйте ещё раз."}
+                </h2>
+            </div>
+        </div>
+    );
+}
+
 //Вы успешно зарегистрировались 
 //Что-то пошло не так! Попробуйте ешё раз
