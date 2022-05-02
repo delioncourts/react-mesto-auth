3cb7ff3f0d60821581d0fbf8f87d78473cc93a9e
delioncourts <delioncourtles@yandex.ru>
2022-05-02 20:06:03 +0300

    upd HeaderInfo plus css

---
+++ src/components/HeaderInfo.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import React from 'react';
+
+function HeaderInfo({ loggedIn, email, onSignOut }) {
+  return (
+    <div className='header__info'>
+      <p className='header__email'>{email}</p>
+      <Link to='sign-up' className={`header__link ${loggedIn && 'header__link_active'}`} onClick={onSignOut}>
+        Выйти
+      </Link>
+    </div>
+  )
+}
+
+export default HeaderInfo;
